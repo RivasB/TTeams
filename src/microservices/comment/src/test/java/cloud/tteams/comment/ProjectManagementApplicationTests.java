03e2c65acf30a6ce01523efa55b47557e99d4557
David Alejandro <david.rivas@externos.seresco.es>
2023-10-21 16:18:26 -0400

    feature: creating comment-microservice major structure and microservice basic skeleton

--- src/microservices/comment/src/test/java/cloud/tteams/comment/ProjectManagementApplicationTests.java
+++ src/microservices/comment/src/test/java/cloud/tteams/comment/ProjectManagementApplicationTests.java
@@ -1,11 +1,10 @@
 package cloud.tteams.comment;
-import cloud.tteams.comment.comment.domain.service.ProjectDomainServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ProjectDomainServiceTest.class})
+//@Suite.SuiteClasses({ProjectDomainServiceTest.class})
 @SpringBootTest
 public class ProjectManagementApplicationTests {
 }
