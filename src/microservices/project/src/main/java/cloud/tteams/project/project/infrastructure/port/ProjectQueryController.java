7fdedac4df795740101c4df0355c3ff8d5fe79c3
David Alejando <ucirivas@gmail.com>
2025-04-01 16:52:09 -0400

    hotfix: update project API endpoints and improve equality checks

--- src/microservices/project/src/main/java/cloud/tteams/project/project/infrastructure/port/ProjectQueryController.java
+++ src/microservices/project/src/main/java/cloud/tteams/project/project/infrastructure/port/ProjectQueryController.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/project")
+@RequestMapping("/api/v1/project")
 @SecurityRequirement(name = "Bearer Authentication")
 @Tag(name = "Query-Project", description = " Query Project API. Contains the query operations that can " +
         "be performed on a Project.")
--- src/microservices/project/src/main/java/cloud/tteams/project/project/infrastructure/service/ProjectDomainServiceImplementation.java
+++ src/microservices/project/src/main/java/cloud/tteams/project/project/infrastructure/service/ProjectDomainServiceImplementation.java
@@ -10,6 +10,7 @@ import cloud.tteams.project.project.domain.valueobject.ProjectId;
 import cloud.tteams.project.project.domain.repository.IProjectCommandRepository;
 import cloud.tteams.project.project.domain.repository.IProjectQueryRepository;
 import cloud.tteams.project.project.domain.service.IProjectDomainService;
+import jakarta.transaction.Transactional;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -48,6 +49,7 @@ public class ProjectDomainServiceImplementation implements IProjectDomainService
     }
 
     @Override
+    @Transactional
     public void update(Project project) {
         Project updated = commandRepository.update(project);
         logger.info(
