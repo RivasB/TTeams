884cc2ad3803528be24c5e29c24681450464bd64
David Alejando <ucirivas@gmail.com>
2025-04-10 13:49:19 -0400

    hotfix: rename gateway.properties to gateway.yaml and restructure configuration

--- src/cloudservices/gateway/src/main/java/cloud/tteams/gateway/GatewayApplication.java
+++ src/cloudservices/gateway/src/main/java/cloud/tteams/gateway/GatewayApplication.java
@@ -3,10 +3,7 @@ package cloud.tteams.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-
 @SpringBootApplication
-@OpenAPIDefinition
 public class GatewayApplication {
 
 	public static void main(String[] args) {
